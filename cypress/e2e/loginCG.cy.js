7a1aa831be83a0ff3f5e30307212e02fc87dd235
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2024-09-25 22:25:37 -0400

    Create a login test

--- cypress/e2e/loginCG.cy.js
+++ cypress/e2e/loginCG.cy.js
@@ -1,5 +1,19 @@
+import {login} from "../../cypress/support/pom/LoginPage";
+import LoginSelectors from "../../cypress/fixtures/loginSelectors.json";
+
+const { username, password } = Cypress.env("userCredentials");
+
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  beforeEach("get the login url", () => {
+    login.get.endpoint()
+    
+    cy.url().should("include", "https://staging.chicksgold.com")
+  });
+
+  it('full fill login form', () => {
+    cy.wait(5000);
+    cy.get(LoginSelectors.buttonSingInCG).should("have.text", "Sign In").click();
+
+    login.fullFillLoginForm(username, password)
+  });
+});
