b8137514b22c88b9c17668797daf1b7b1250493a
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2024-09-27 21:05:02 -0400

    Create navBarAndFooter.cy.js

---
+++ cypress/e2e/navBarAndFooter.cy.js
@@ -0,0 +1,21 @@
+import {login} from "../../cypress/support/pom/LoginPage";
+import {homePage} from "../../cypress/support/pom/HomePage";
+
+describe('template spec', () => {
+    beforeEach("get the login url", () => {
+        cy.clearCookies();
+        cy.clearLocalStorage();
+        cy.viewport(1920, 1080)
+        login.visitCG()
+      
+        cy.url().should("include", "https://staging.chicksgold.com")
+    });
+    
+    it('Home Page NavBar Hover', () => homePage.hoverNavBarElements());
+    
+    it('Home Page NavBar Links', () => homePage.clickNavBarElements());
+  
+    it('Home Page Footer', () => homePage.clickCGFooterElements());
+    
+  });
+  
